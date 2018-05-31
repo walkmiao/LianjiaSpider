bf37ed1d898669e3ee8aca5e36ff19268e53e41e
walkmiao <372815340@qq.com>
2018-05-31 19:54:16 +0800

    无需在创建目录的时候自己创建 file_path会自动创建

--- Lianjia/imagepipelines.py
+++ Lianjia/imagepipelines.py
@@ -8,11 +8,13 @@ import scrapy
 from scrapy.contrib.pipeline.images import ImagesPipeline
 from scrapy.exceptions import DropItem
 import os,logging
+import codecs
 
 class MyImagesPipeline(ImagesPipeline):
 
     def get_media_requests(self, item, info):
         for image_url in item['image_urls']:
+            #添加referer，和meta参数，meta把item传入以便file_path能在request中获取item
             yield scrapy.Request(url=image_url, headers={'Referer': item['header_referer'],
                                                          'Agent': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8'},
                                  meta={'item':item})
@@ -29,15 +31,18 @@ class MyImagesPipeline(ImagesPipeline):
 
     def file_path(self, request, response=None, info=None):
         item = request.meta['item']
-        image_dir_name = r"/tmp/images/" + item['region'] + '/' + item['plot']
-        if not os.path.exists(image_dir_name):
-            try:
-                os.makedirs(name=image_dir_name, mode=0o777)
-                logging.debug('[SUCC] 创建%s目录成功！' % image_dir_name )
-            except Exception as e:
-                logging.debug('创建%s目录失败! [异常原因]:%s' % (image_dir_name, e))
-        else:
-            pass
+        #file_path 不需要自己创建dir，函数会自己按照return的路径来帮我们创建好
 
-        image_name = r"%s/%s/%s.jpg" % (item['region'], item['plot'], request.url.replace('/',''))
+        # image_dir_name = r"/tmp/images/" + item['region'] + '/' + item['plot']
+        # if not os.path.exists(image_dir_name):
+        #     try:
+        #         #创建目录
+        #         os.makedirs(name=image_dir_name, mode=0o777)
+        #         logging.debug('[SUCC] 创建%s目录成功！' % image_dir_name )
+        #     except Exception as e:
+        #         logging.debug('创建%s目录失败! [异常原因]:%s' % (image_dir_name, e))
+        # else:
+        #     pass
+        #request.url中含有/会错误的再创建目录
+        image_name = r"%s/%s/%s.jpg" % (item['region'], item['plot'], request.url.replace('//','').replace('/',''))
         return image_name
